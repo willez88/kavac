66635d6af7e81f0a5cae65e0f36133ffb6efd898
Ing. Roldan Vargas <roldandvg@gmail.com>
2018-03-31 13:00:28 -0400

    agregado modelo para la gestión de imágenes del sistema

---
+++ database/migrations/2018_03_31_165038_create_images_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateImagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('images')) {
+            Schema::create('images', function (Blueprint $table) {
+                $table->increments('id')->comment('Identificador único del registro');
+                $table->string('file')->comment('Nombre del archivo');
+                $table->string('url')->comment('URL de la imagen');
+                $table->timestamps();
+                $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('images');
+    }
+}
