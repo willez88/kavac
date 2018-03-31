9a5d3d3328b517e3389733dc35335b307197e1f5
Ing. Roldan Vargas <roldandvg@gmail.com>
2018-03-31 11:23:58 -0400

    agregado service providers para ultraware/roles

--- config/app.php
+++ config/app.php
@@ -177,6 +177,11 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /**
+         * Third Party Service Providers...
+         */
+        Ultraware\Roles\RolesServiceProvider::class,
+
     ],
 
     /*
