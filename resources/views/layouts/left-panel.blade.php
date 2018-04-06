f541f02c6a6d30309e072309daaa94feab9d1b59
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-04-05 22:07:56 -0400

    menu de opción para la gestión de contabilidad

--- resources/views/layouts/left-panel.blade.php
+++ resources/views/layouts/left-panel.blade.php
@@ -166,31 +166,7 @@
                     </li>
                 </ul>
             </li>
-            {{-- Gestión de contabilidad --}}
-            <li>
-                <a href="#" title="Gestión de asientos contables" data-toggle="tooltip" data-placement="right">
-                    <i class="ion-social-buffer-outline"></i><span>Contabilidad</span>
-                </a>
-                <ul class="submenu">
-                    <li>
-                        <a href="#">Cuentas Patrimoniales</a>
-                    </li>
-                    <li>
-                        <a href="#">Convertidor de Cuentas</a>
-                    </li>
-                    <li>
-                        <a href="#">Asiento Contable</a>
-                    </li>
-                    <li>
-                        <a href="#">Reportes</a>
-                        <ul class="submenu">
-                            <li><a href="#">Mayor Analítico</a></li>
-                            <li><a href="#">Libro Diario</a></li>
-                            <li><a href="#">Libro Auxiliar</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+            @include('accounting::layouts.menu-option')
             {{-- Gestión de cuentas por pagar --}}
             <li>
                 <a href="#" title="Gestión de cuentas por pagar" data-toggle="tooltip" data-placement="right">
