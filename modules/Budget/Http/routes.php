16cf73a90966f0b6d80bcae3f3620406ab8a7b59
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-04-05 22:12:17 -0400

    módulo para la gestión de presupuesto

---
+++ modules/Budget/Http/routes.php
@@ -0,0 +1,6 @@
+<?php
+
+Route::group(['middleware' => 'web', 'prefix' => 'budget', 'namespace' => 'Modules\Budget\Http\Controllers'], function()
+{
+    Route::get('/', 'BudgetController@index');
+});
--- resources/views/layouts/left-panel.blade.php
+++ resources/views/layouts/left-panel.blade.php
@@ -40,34 +40,7 @@
                     <i class="ion-social-dropbox-outline"></i><span>Proveedores</span>
                 </a>
             </li>
-            {{-- Gestión de presupuesto --}}
-            <li>
-                <a href="#" title="Formulación y ejecución del presupuesto" data-toggle="tooltip" 
-                   data-placement="right">
-                    <i class="ion-arrow-graph-up-right"></i><span>Presupuesto</span>
-                </a>
-                <ul class="submenu">
-                    <li>
-                        <a href="#">Configuración</a>
-                    </li>
-                    <li>
-                        <a href="#">Plan de Cuentas</a>
-                    </li>
-                    <li>
-                        <a href="#">Formulación</a>
-                    </li>
-                    <li>
-                        <a href="#">Ejecución</a>
-                    </li>
-                    <li>
-                        <a href="#">Reportes</a>
-                        <ul class="submenu">
-                            <li><a href="#">Mayor Análitico</a></li>
-                            <li><a href="#">Consolidado</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
+            @include('budget::layouts.menu-option')
             {{-- Gestión de compras --}}
             <li>
                 <a href="#" title="Gestión de compras de bienes y servicios" data-toggle="tooltip" 
