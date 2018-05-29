a013ba33b6f92e549320d25f50ccee1f05549e6f
William Páez <wpaez@cenditel.gob.ve>
2018-05-29 08:53:30 -0400

    rama creada para el módulo de presupuesto

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,9 +65,9 @@ return [
             'driver' => 'pgsql',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'kavac'),
+            'username' => env('DB_USERNAME', 'admin'),
+            'password' => env('DB_PASSWORD', '123'),
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
@@ -78,7 +78,7 @@ return [
                 'timeout' => 60 * 20, // 20 minute timeout
                 //'exclude_tables' => ['table1', 'table2'],
                 'add_extra_option' => '-o -b -Ft',
-            ]  
+            ]
         ],
 
         'sqlsrv' => [
