5f17d34088e93d1b04968931c5f323788e8e25fb
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-09-12 14:33:08 -0400

    agregada descripción breve en permisos de presupuesto

--- modules/Budget/Database/Seeders/BudgetRoleAndPermissionsTableSeeder.php
+++ modules/Budget/Database/Seeders/BudgetRoleAndPermissionsTableSeeder.php
@@ -21,7 +21,7 @@ class BudgetRoleAndPermissionsTableSeeder extends Seeder
         $adminRole = Role::where('slug', 'admin')->first();
 
         $budgetRole = Role::updateOrCreate(
-            ['slug' => 'Budget'],
+            ['slug' => 'budget'],
             ['name' => 'Presupuesto', 'description' => 'Coordinador de presupuesto']
         );
 
@@ -30,61 +30,62 @@ class BudgetRoleAndPermissionsTableSeeder extends Seeder
                 'name' => 'Inicio del módulo de presupuesto', 'slug' => 'budget.home',
                 'description' => 'Acceso a descripción del módulo de presupuesto', 
                 'model' => 'Modules\Budget\Models\BudgetAccount', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'presupuesto.inicio'
+                'slug_alt' => 'presupuesto.inicio', 'short_description' => 'página de inicio'
             ],
             [
                 'name' => 'Configuración del módulo de presupuesto', 'slug' => 'budget.setting.create',
                 'description' => 'Acceso a la configuración del módulo de presupuesto', 
                 'model' => '', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'configuracion.crear'
+                'slug_alt' => 'configuracion.crear', 'short_description' => 'agregar configuración'
             ],
             [
                 'name' => 'Editar configuración del módulo de presupuesto', 
                 'slug' => 'budget.setting.edit',
                 'description' => 'Acceso para editar la configuración del módulo de presupuesto', 
                 'model' => '', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'configuracion.editar'
+                'slug_alt' => 'configuracion.editar', 'short_description' => 'editar configuración'
             ],
             [
                 'name' => 'Ver configuración del módulo de presupuesto', 
                 'slug' => 'budget.setting.list',
                 'description' => 'Acceso para editar la configuración del módulo de presupuesto', 
                 'model' => '', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'configuracion.ver'
+                'slug_alt' => 'configuracion.ver', 'short_description' => 'ver configuración'
             ],
             [
                 'name' => 'Eliminar configuración del módulo de presupuesto', 
                 'slug' => 'budget.setting.delete',
                 'description' => 'Acceso para eliminar la configuración del módulo de presupuesto', 
                 'model' => '', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'configuracion.eliminar'
+                'slug_alt' => 'configuracion.eliminar', 'short_description' => 'eliminar configuración'
             ],
             [
                 'name' => 'Crear cuenta presupuestaria', 'slug' => 'budget.account.create',
                 'description' => 'Acceso para crear cuenta presupuestaria', 
                 'model' => 'Modules\Budget\Models\BudgetAccount', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'clasificador.crear'
+                'slug_alt' => 'clasificador.crear', 'short_description' => 'agregar clasificador'
             ],
             [
                 'name' => 'Editar cuenta presupuestaria', 'slug' => 'budget.account.edit',
                 'description' => 'Acceso para editar cuenta presupuestaria', 
                 'model' => 'Modules\Budget\Models\BudgetAccount', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'clasificador.editar'
+                'slug_alt' => 'clasificador.editar', 'short_description' => 'editar clasificador'
             ],
             [
                 'name' => 'Eliminar cuenta presupuestaria', 'slug' => 'budget.account.delete',
                 'description' => 'Acceso para eliminar cuenta presupuestaria', 
                 'model' => 'Modules\Budget\Models\BudgetAccount', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'clasificador.eliminar'
+                'slug_alt' => 'clasificador.eliminar', 'short_description' => 'eliminar clasificador'
             ],
             [
                 'name' => 'Ver cuentas presupuestarias', 'slug' => 'budget.account.list',
                 'description' => 'Acceso para ver cuentas presupuestarias', 
                 'model' => 'Modules\Budget\Models\BudgetAccount', 'model_prefix' => 'presupuesto',
-                'slug_alt' => 'clasificador.ver'
+                'slug_alt' => 'clasificador.ver', 'short_description' => 'ver clasificador'
             ],
         ];
 
+        $budgetRole->detachAllPermissions();
 
         foreach ($permissions as $permission) {
             $per = Permission::updateOrCreate(
@@ -92,7 +93,7 @@ class BudgetRoleAndPermissionsTableSeeder extends Seeder
                 [
                     'name' => $permission['name'], 'description' => $permission['description'],
                     'model' => $permission['model'], 'model_prefix' => $permission['model_prefix'],
-                    'slug_alt' => $permission['slug_alt']
+                    'slug_alt' => $permission['slug_alt'], 'short_description' => $permission['short_description']
                 ]
             );
 
