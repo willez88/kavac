7db15ccbedf1a504b195308b109888980de6d53b
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-09-12 14:32:39 -0400

    nuevo campo para descripción breve en permisos

---
+++ database/migrations/2018_09_12_144844_add_field_short_description_to_permissions_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldShortDescriptionToPermissionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->string('short_description')->default('')->comment('Descripción breve del permiso');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('permissions', function (Blueprint $table) {
+            $table->dropColumn('short_description');
+        });
+    }
+}
