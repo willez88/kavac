5d8e223eedca903cc688a7f628eb5603a70b05c4
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-09-18 12:49:40 -0400

    migración ara la configuración de códigos personalizados en tablas

---
+++ app/Models/CodeSetting.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use \Venturecraft\Revisionable\RevisionableTrait;
+
+class CodeSetting extends Model
+{
+    use SoftDeletes;
+    use RevisionableTrait;
+
+    /**
+     * Establece el uso o no de bitácora de registros para este modelo
+     * @var boolean $revisionCreationsEnabled
+     */
+    protected $revisionCreationsEnabled = true;
+
+    /**
+     * Lista de atributos para la gestión de fechas
+     * @var array $dates
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Lista de atributos que pueden ser asignados masivamente
+     * @var array $fillable
+     */
+    protected $fillable = [
+    	'module', 'table', 'field', 'active', 'format_prefix', 'format_digits', 'format_year', 'description'
+    ];
+}
---
+++ database/migrations/2018_09_18_161506_create_code_settings_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCodeSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('code_settings')) {
+            Schema::create('code_settings', function (Blueprint $table) {
+                $table->increments('id');
+                $table->string('module')->nullable()->comment('Nombre del módulo si aplica');
+                $table->string('table')->comment('Tabla en donde se aplicará la configuración del código');
+                $table->string('field')->default('code')
+                      ->comment('Nombre del campo que registrará el código configurado. El campo por defecto es "code"');
+                $table->boolean('active')->default(true)->comment('Indica si la configuración esta activa');
+                $table->string('format_prefix', 3)->nullable()->comment('Formato del prefijo configurado');
+                $table->string('format_digits', 8)->comment('Formato de los dígitos configurados');
+                $table->enum('format_year', ['YY', 'YYYY'])->nullable()->comment('Formato del prefijo configurado');
+                $table->text('description')->nullable()->comment('Descripción de la configuración del código');
+                $table->timestamps();
+                $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
+                $table->unique([
+                    'module', 'table', 'field', 'active', 'format_prefix', 'format_digits', 'format_year'
+                ])->comment('Clave única para el registro');
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('code_settings');
+    }
+}
