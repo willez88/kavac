186463e0e6ffe240c8132f8403fcb89444b533b6
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-09-19 21:34:04 -0400

    instrucciones para requerir componentes vue de los módulos

--- resources/assets/js/modules.js
+++ resources/assets/js/modules.js
@@ -3,5 +3,24 @@
  *
  * @author  Ing. Roldan Vargas <rvargas at cenditel.gob.ve>
  */
+
+/** Compilación de componentes y configuración vuejs para el módulo de presupuesto */
 require('../../../modules/Budget/Resources/assets/js/_all.js');
+
+/** Compilación de componentes y configuración vuejs para el módulo de bienes */
 require('../../../modules/Asset/Resources/assets/js/_all.js');
+
+/** Compilación de componentes y configuración vuejs para el módulo de nómina */
+require('../../../modules/Payroll/Resources/assets/js/_all.js');
+
+/** Compilación de componentes y configuración vuejs para el módulo de contabilidad */
+require('../../../modules/Accounting/Resources/assets/js/_all.js');
+
+/** Compilación de componentes y configuración vuejs para el módulo de firma electrónica */
+require('../../../modules/DigitalSignature/Resources/assets/js/_all.js');
+
+/** Compilación de componentes y configuración vuejs para el módulo de compras */
+require('../../../modules/Purchase/Resources/assets/js/_all.js');
+
+/** Compilación de componentes y configuración vuejs para el módulo de almacén */
+require('../../../modules/Warehouse/Resources/assets/js/_all.js');
