afc9d0b6e879212ee43c7e25d47d23b6d211e39c
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2018-09-26 15:03:28 -0400

    modelo y migración para la gestión de número telefónicos

---
+++ app/Models/Phone.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use \Venturecraft\Revisionable\RevisionableTrait;
+
+class Phone extends Model
+{
+    use SoftDeletes;
+    use RevisionableTrait;
+
+    /**
+     * Establece el uso o no de bitácora de registros para este modelo
+     * @var boolean $revisionCreationsEnabled
+     */
+    protected $revisionCreationsEnabled = true;
+
+    /**
+     * Lista de atributos para la gestión de fechas
+     * @var array $dates
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Lista de atributos que pueden ser asignados masivamente
+     * @var array $fillable
+     */
+    protected $fillable = ['area_code', 'number', 'type', 'extension'];
+}
---
+++ database/migrations/2018_09_26_185743_create_phones_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePhonesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('phones', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('area_code')->comment('Código de área');
+            $table->string('number')->comment('Número telefónico');
+            $table->enum('type', ['M', 'T', 'F'])->default('T')->comment('Tipo de teléfono: (M)óvil, (T)eléfono, (F)ax');
+            $table->string('extension')->nullable()->comment('Número de extensión (si posee)');
+            $table->timestamps();
+            $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('phones');
+    }
+}
