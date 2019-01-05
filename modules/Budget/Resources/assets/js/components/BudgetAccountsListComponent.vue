576b1e8e39e47b76ddac3f7998e236387b9f1c6b
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-01-05 11:12:55 -0400

    modificación en tabla que muestra las cuentas presupuestarias, se cambió a vue-table-2

--- modules/Budget/Resources/assets/js/components/BudgetAccountsListComponent.vue
+++ modules/Budget/Resources/assets/js/components/BudgetAccountsListComponent.vue
@@ -14,36 +14,6 @@
 			</button>
 		</div>
 	</v-client-table>
-	<!--<table class="table table-big table-hover table-striped dt-responsive nowrap datatable">
-		<thead>
-			<tr class="text-center">
-				<th>Código</th>
-				<th>Denominación</th>
-				<th>Original</th>
-				<th>Acción</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr v-for="(rec, index) in records">
-				<td width="20%">
-					{{ rec.group + "." + rec.item + "." + rec.generic + "." + rec.specific + "." + rec.subspecific }}
-				</td>
-				<td width="60%" class="wrapping">{{ rec.denomination }}</td>
-				<td class="text-center" width="10%">{{ (rec.original)?'SI':'NO' }}</td>
-				<td class="text-center" width="10%">
-					<button @click="editForm(rec.id)" 
-							class="btn btn-warning btn-xs btn-icon btn-round" 
-							title="Modificar registro" data-toggle="tooltip" type="button">
-						<i class="fa fa-edit"></i>
-					</button>
-					<button @click="deleteRecord(index, '')" 
-							class="btn btn-danger btn-xs btn-icon btn-round" title="Eliminar registro" data-toggle="tooltip" type="button">
-						<i class="fa fa-trash-o"></i>
-					</button>
-				</td>
-			</tr>
-		</tbody>
-	</table>-->
 </template>
 
 <script>
