08f77f48d551edb439441fa8a9335e9d56f49799
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-01-21 11:00:04 -0400

    corrección en clave foránea

--- modules/Budget/Database/Migrations/2019_01_13_172329_create_budget_sub_specific_formulations_table.php
+++ modules/Budget/Database/Migrations/2019_01_13_172329_create_budget_sub_specific_formulations_table.php
@@ -26,7 +26,7 @@ class CreateBudgetSubSpecificFormulationsTable extends Migration
                       ->comment('Identificador asociado a la acción específica de la formulación');
                 $table->timestamps();
                 $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
-                $table->foreign('currencies_id')->references('id')->on('currencies')->onUpdate('cascade');
+                $table->foreign('currency_id')->references('id')->on('currencies')->onUpdate('cascade');
                 $table->foreign('budget_specific_action_id')->references('id')
                       ->on('budget_specific_actions')->onUpdate('cascade');
             });
