a0c998870ac708ef4b860c83fb8ac73b6f93dc78
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-01-23 22:19:46 -0400

    agregado campo para registrar el código generado para la formulación

---
+++ modules/Budget/Database/Migrations/2019_01_23_182534_add_field_code_to_budget_sub_specific_formulations_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldCodeToBudgetSubSpecificFormulationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('budget_sub_specific_formulations', function (Blueprint $table) {
+            $table->string('code', 20)->unique()->comment('Código único para la formulación');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('budget_sub_specific_formulations', function (Blueprint $table) {
+            $table->dropColumn('code');
+        });
+    }
+}
