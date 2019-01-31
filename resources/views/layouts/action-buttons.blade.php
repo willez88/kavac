e3cf5a38f7d52e584e97a9a7380d6c65be54776b
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-01-30 21:39:27 -0400

    agregada plantilla para botones de acciones en los registros

---
+++ resources/views/layouts/action-buttons.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="col-12 text-right">
+		@if (isset($print))
+			{!! Form::button('<i class="fa fa-print"></i>', [
+				'class' => 'btn btn-sm btn-primary btn-custom', 'data-toggle' => 'tooltip', 'type' => 'button',
+				'title' => 'Imprimir registro', 'onclick' => (isset($print['action']))?$print['action']:'print()'
+			]) !!}
+		@endif
+	</div>
+</div>
