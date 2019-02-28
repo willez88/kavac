570ff09119a4f0e6663c8c45de22bc86ac95043f
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-02-27 21:20:44 -0400

    plantilla para botón de uso común para agregar nuevos registros

---
+++ resources/views/layouts/button-new-register.blade.php
@@ -0,0 +1,10 @@
+<div class="row">
+	<div class="col-12">
+		<a href="{{ $url ?? '#' }}" 
+		   class="btn btn-sm btn-primary btn-custom float-right" 
+		   title="Crear nuevo registro" data-toggle="tooltip">
+			<i class="fa fa-plus-circle"></i>
+			<span>{{ $btnText ?? 'Nuevo' }}</span>
+		</a>
+	</div>
+</div>
