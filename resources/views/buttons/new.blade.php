6c65caba07a8fc4dbac429a6b1e35a61967b3a2e
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-03-06 14:04:56 -0400

    botones de uso común

---
+++ resources/views/buttons/new.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ $route }}" class="btn btn-sm btn-primary btn-custom" title="Crear nuevo registro" data-toggle="tooltip">
+	<i class="fa fa-plus-circle"></i>
+</a>
---
+++ resources/views/buttons/previous.blade.php
@@ -0,0 +1,3 @@
+<a href="{{ $route }}" class="btn btn-sm btn-primary btn-custom" title="Ir atrás" data-toggle="tooltip">
+	<i class="fa fa-reply"></i>
+</a>
