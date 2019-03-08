6f9157cfb725b8df155f61b0ab8a8f204a5f17e0
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-03-08 09:56:58 -0400

    mensaje de validación del captcha

--- resources/lang/es/validation.php
+++ resources/lang/es/validation.php
@@ -87,6 +87,7 @@ return [
     'unique' => ':attribute ya ha sido registrado.',
     'uploaded' => 'Subir :attribute ha fallado.',
     'url' => 'El formato :attribute es inválido.',
+    'captcha' => 'El captcha es incorrecto',
 
     /*
     |--------------------------------------------------------------------------
