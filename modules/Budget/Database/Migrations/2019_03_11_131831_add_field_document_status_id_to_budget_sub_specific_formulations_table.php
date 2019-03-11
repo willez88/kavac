d48a7de17b8fcfe489cbf248bab55c39f524d79c
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-03-11 10:38:00 -0400

    agregado campo de estatus de documento

---
+++ modules/Budget/Database/Migrations/2019_03_11_131831_add_field_document_status_id_to_budget_sub_specific_formulations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldDocumentStatusIdToBudgetSubSpecificFormulationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('budget_sub_specific_formulations', function (Blueprint $table) {
+            $table->integer('document_status_id')->unsigned()->nullable()
+                  ->comment('Identificador asociado a la institución');
+            $table->foreign('document_status_id')->references('id')->on('document_status')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('budget_sub_specific_formulations', function (Blueprint $table) {
+            $table->dropForeign('budget_sub_specific_formulations_document_status_id_foreign');
+            $table->dropColumn('document_status_id');
+        });
+    }
+}
