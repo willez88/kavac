4e908d9bffe7c464bec332762d1df27a77b3435e
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-03-19 14:59:35 -0400

    comentario agregado para identificar las distintas acciones posibles del campo action

--- database/migrations/2019_02_06_221844_add_field_action_to_document_status_table.php
+++ database/migrations/2019_02_06_221844_add_field_action_to_document_status_table.php
@@ -15,7 +15,10 @@ class AddFieldActionToDocumentStatusTable extends Migration
     {
         Schema::table('document_status', function (Blueprint $table) {
             $table->enum('action', ['AP', 'RE', 'EL', 'PR', 'AN'])->default('AP')
-                  ->comment('Acción que realiza el estado del documento');
+                  ->comment(
+                    'Acción que realiza el estado del documento. Valores aceptados: (AP)robado, (RE)chazado, ' .
+                    '(EL)aborado, (PR)or revisar, (AN)ulado'
+                  );
         });
     }
 
