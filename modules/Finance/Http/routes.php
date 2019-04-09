6a7754f7546fbaba731546c7395752151cce15f4
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-04-09 16:23:13 -0400

    corrección en ruta para registrar configuración

--- modules/Finance/Http/routes.php
+++ modules/Finance/Http/routes.php
@@ -10,7 +10,7 @@ Route::group([
     Route::group(['middleware' => 'permission:finance.setting.create'], function() {
     	/** Ruta de acceso a parámetros de configuración del módulo */
     	Route::get('settings', 'FinanceController@setting')->name('finance.setting.index');
-    	Route::post('settings', 'FinanceConroller@store')->name('finance.setting.store');
+    	Route::post('settings', 'FinanceController@store')->name('finance.setting.store');
 	    /** Rutas para la gestión de entidades bancarias */
 	    Route::resource('banks', 'FinanceBankController', ['as' => 'finance']);
 	    /** Rutas para la gestión de agencias bancarias */
