30a3cbfaa29c556f4928b8fd17a99ea86ff0273f
Ing. Roldan Vargas <roldandvg@gmail.com>
2019-04-09 14:44:22 -0400

    agregado helper para el módulo de finanzas

---
+++ modules/Finance/Helpers/Helper.php
@@ -0,0 +1,16 @@
+<?php
+
+if (! function_exists('format_bank_account')) {
+	function format_bank_account($account, $formated = true)
+	{
+		/*$account_formated = '';
+		for ($i = 0; $i < $account; $i++) {
+			if ($formated && in_array([4, 8, 10], $i) .includes(i) && account.charAt(i) !== "-") {
+                	account_formated += '-';
+                }
+            	account_formated += account.charAt(i);
+            }
+
+            return account_formated;*/
+	}
+}
