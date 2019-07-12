a4ecac6a3f9f052c0cdde1d0dbc120faf80d5856
Juan Rosas <JuanFBass17@gmail.com>
2019-07-12 10:23:43 -0400

    merge sin accounting

--- modules/Accounting/Resources/views/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('accounting::layouts.master')
-
-@section('content')
-    <h1>Hello World</h1>
-
-    <p>
-        This view is loaded from module: {!! config('accounting.name') !!}
-    </p>
-@stop
