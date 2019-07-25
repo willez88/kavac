62b84554ceca2b750ae2a04f3e39e5ecf7a80959
Henry Paredes <henryp2804@gmail.com>
2019-07-25 13:12:14 -0400

    Agregando reglas personalizadas

---
+++ modules/Asset/Rules/AcquisitionYear.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Modules\Asset\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+
+/**
+ * @class AcquisitionYear
+ * @brief Reglas de validación para el año de adquisición de un bien
+ *
+ * Gestiona las reglas de validación de el año de adquisición de un bien
+ *
+ * @author Henry Paredes <hparedes@cenditel.gob.ve>
+ * @license <a href='http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/'>LICENCIA DE SOFTWARE CENDITEL</a>
+ */
+class AcquisitionYear implements Rule
+{
+    /** Integer Define el año maximo de adquisición */
+    protected $year;
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct($year)
+    {
+        $this->year = $year;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return $value <= $this->year;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'El campo :attribute no debe ser superior al año actual. El año actual es: '. $this->year;
+    }
+}
