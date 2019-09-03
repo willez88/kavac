d84589af95dace735392e0ffc330966cc5b6d624
Ing. Roldan Vargas <roldandvg@gmail.com>
2019-09-03 15:00:29 -0400

    agregadas opciones de menú para los módulos de CitizenService y TechnicalSupport

---
+++ modules/CitizenService/Resources/views/layouts/menu-option.blade.php
@@ -0,0 +1,12 @@
+{{-- Gestión de atención al ciudadano --}}
+<li>
+    <a href="#" title="Gestión de atención al ciudadano" data-toggle="tooltip" 
+       data-placement="right">
+        <i class="icofont icofont-users-social"></i><span>Atención / Ciudadano</span>
+    </a>
+    <ul class="submenu" style="{!! display_submenu('citizenservice') !!}">
+        <li>
+            <a href="#">Configuración</a>
+        </li>
+    </ul>
+</li>
