3b22e6f93c01b0ae60526cda1c36aa13e6d78cf7
Henry Paredes <henryp2804@gmail.com>
2019-09-23 10:54:10 -0400

    Agregando instrucciones para optimizar listado de bienes

--- modules/Asset/Http/routes.php
+++ modules/Asset/Http/routes.php
@@ -18,13 +18,13 @@ Route::group(
         /**
          * Rutas para gestionar los registros de bienes
          */
-        #Route::resource('registers', 'AssetController', ['only' => ['store', 'update']]);
+        Route::resource('registers', 'AssetController', ['only' => ['store', 'update']]);
         Route::get('registers', 'AssetController@index')->name('asset.register.index');
         Route::get('registers/create', 'AssetController@create')->name('asset.register.create');
         Route::get('registers/edit/{asset}', 'AssetController@edit')->name('asset.register.edit');
         Route::delete('registers/delete/{asset}', 'AssetController@destroy')->name('asset.register.destroy');
         Route::get('registers/info/{asset}', 'AssetController@vueInfo');
-        Route::get('registers/vue-list', 'AssetController@vueList')->name('asset.register.vuelist');
+        Route::get('registers/vue-list/{perPage?}/{page?}', 'AssetController@vueList')->name('asset.register.vuelist');
         Route::post('registers/search/general', 'AssetController@searchGeneral');
         Route::post('registers/search/clasification', 'AssetController@searchClasification');
         Route::post('registers/search/dependence', 'AssetController@searchDependence');
