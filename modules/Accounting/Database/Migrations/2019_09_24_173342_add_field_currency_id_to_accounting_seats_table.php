14f72d97a9af8a2ddd05d26de1d040664ace52f8
Juan Rosas <JuanFBass17@gmail.com>
2019-09-24 15:38:52 -0400

    manejo de moneda por asiento contable

---
+++ modules/Accounting/Database/Migrations/2019_09_24_173342_add_field_currency_id_to_accounting_seats_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddFieldCurrencyIdToAccountingSeatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('accounting_seats', function (Blueprint $table) {
+            if (!Schema::hasColumn('accounting_seats', 'currency_id')) {
+                $table->integer('currency_id')->unsigned()->nullable()
+                ->comment('id del tipo de moneda en que se guardar el asiento contable');
+                
+                $table->foreign('currency_id')->references('id')->on('currencies')
+                ->onDelete('cascade')->comment('id del tipo de moneda en que se guardar el asiento contable');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('accounting_seats', function (Blueprint $table) {
+            if (Schema::hasColumn('accounting_seats', 'currency_id')) {
+                $table->dropColumn('currency_id');
+            }
+        });
+    }
+}
