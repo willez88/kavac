b82bbff5bffb060aa5cb5e3bc8d87d6bbce29a92
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-10-02 22:40:29 -0400

    agregada plantilla para botón de ayuda que ejecuta una vista guiada de los elementos del componente a solicitar

---
+++ resources/views/buttons/help.blade.php
@@ -0,0 +1,17 @@
+<a href="javascript:void(0)" title="haz click para ver la ayuda guiada de este elemento"
+   data-toggle="tooltip" class="btn-help" id="btnHelp{{ $helpId ?? '' }}">
+    <i class="ion ion-ios-help-outline cursor-pointer"></i>
+</a>
+
+@section('extra-js')
+    @parent
+    <script>
+        $(document).ready(function() {
+            @if (isset($helpId) && !empty($helpId) && isset($helpSteps) && is_array($helpSteps) && count($helpSteps) > 0)
+                $('#btnHelp{{ $helpId }}').on('click', function() {
+                    startGuidedTour(JSON.parse(JSON.stringify({!! json_encode($helpSteps) !!})));
+                });
+            @endif
+        });
+    </script>
+@stop
