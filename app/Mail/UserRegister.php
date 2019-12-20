e8371547b7badc028b737f32269097867d3be5ca
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-12-19 22:43:04 -0400

    agregada estructura para el envío de correo electrónico para la gestión en registro de usuarios

---
+++ app/Mail/UserRegister.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+//use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+
+class UserRegister extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $user;
+    public $password;
+    public $appName;
+    public $appUrl;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $password)
+    {
+        $this->user = $user;
+        $this->passwword = $password;
+        $this->appName = config('app.name');
+        $this->appUrl = config('app.url');
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->subject(config('app.name') . " - Registro")->view('emails.user-register');
+    }
+}
