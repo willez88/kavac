2151bc28ec0a471fb006bc37f1f269e5c85c2807
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2019-12-28 08:19:00 -0400

    agregada notificación de información al registrar usuarios en el sistema

---
+++ app/Notifications/UserRegistered.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class UserRegistered extends Notification
+{
+    use Queueable;
+
+    public $user;
+    public $password;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user, $password)
+    {
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->from(config('mail.from.address'), config('mail.from.name'))
+                    ->subject('Registro de usuario - ' . config('app.name'))
+                    ->greeting('Bienvenido, ' . $this->user->name)
+                    ->line('Se ha registrado un usuario en la plataforma con las siguientes credenciales de acceso:')
+                    ->line('Usuario: ' . $this->user->username)
+                    ->line('Contraseña: ' . $this->password)
+                    ->line('Para acceder pulse sobre el botón a continuación')
+                    ->action('Acceso', route('index'))
+                    ->line('Este correo es enviado de manera automática por la aplicación y no esta monitoreado!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
