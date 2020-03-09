d14949c0c940e69d473d3473e96785fd3284f9b7
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2020-03-09 10:18:36 -0400

    migración para agregar campos a la tabla budget_compromises

---
+++ modules/Budget/Database/Migrations/2020_03_09_131632_update_fields_to_budget_compromises_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateFieldsToBudgetCompromisesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('budget_compromises', function (Blueprint $table) {
+            if (!Schema::hasColumn('budget_compromises', 'document_number')) {
+                $table->string('document_number')->unique()
+                      ->comment(<<<EOT
+                          Número del documento que identifica el compromiso. Si es manual se agrega el número indicado 
+                          en el campo Documento Origen, de lo contrarió si proviene de otro proceso se coloca el código 
+                          del documento
+                      EOT);
+            }
+
+            if (!Schema::hasColumn('budget_compromises', 'institution_id')) {
+                $table->bigInteger('institution_id')->unsigned()->nullable()
+                      ->comment('Identificador asociado a la institución');
+                $table->foreign('institution_id')->references('id')
+                      ->on('institutions')->onUpdate('cascade');
+            }
+
+            $table->dropMorphs('sourceable');
+        });
+
+        Schema::table('budget_compromises', function (Blueprint $table) {
+            $table->nullableMorphs('sourceable');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('budget_compromises', function (Blueprint $table) {
+            if (Schema::hasColumn('budget_compromises', 'document_number')) {
+                $table->dropColumn('document_number');
+            }
+            if (Schema::hasColumn('budget_compromises', 'institution_id')) {
+                $table->dropForeign('budget_compromises_institution_id_foreign');
+                $table->dropColumn('institution_id');
+            }
+            $table->dropMorphs('sourceable');
+        });
+
+        Schema::table('budget_compromises', function (Blueprint $table) {
+            $table->morphs('sourceable');
+        });
+    }
+}
