e5f6deb86241a659c3edc3154392a63d0f31560c
Daniel Contreras <exodiadaniel@gmail.com>
2020-03-11 11:40:53 -0400

    Agregado comercialización a la barra de navegación izquierda

---
+++ modules/Sale/Resources/views/layouts/menu-option.blade.php
@@ -0,0 +1,19 @@
+{{-- Gestión de comercialización --}}
+<li>
+    <a href="#" title="Gestión de comercialización" data-toggle="tooltip" data-placement="right">
+        <i class="ion ion-briefcase"></i><span>Comercialización</span>
+    </a>
+    <ul class="submenu" style="{!! display_submenu('sale') !!}">
+        <li>
+            <a href="#">
+                <i></i>Configuración
+            </a>
+        </li>
+        <li>
+            <a href="#">Facturas</a>
+        </li>
+        <li>
+            <a href="#">Reportes</a>
+        </li>
+    </ul>
+</li>
