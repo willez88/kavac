516ce2099b1549c6ae901aa470c1136eac281c58
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2020-03-15 12:39:02 -0400

    agregado observer que monitorea los eventos created, updated, deleted y forceDeleted de los modelos del sistema configurados para notificaciones del sistema

---
+++ app/Observers/ModelObserver.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Observers;
+
+use App\Notifications\SystemNotification;
+use App\Models\NotificationSetting;
+
+class ModelObserver
+{
+    /**
+     * Gestiona el evento "created" de un modelo
+     *
+     * @method     created
+     *
+     * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     *
+     * @param      object           $model    Objeto con información del modelo que genera el evento
+     */
+    public function created($model)
+    {
+        $this->setNotifications($model, 'created');
+    }
+
+    /**
+     * Gestiona el evento "updated" de un modelo
+     *
+     * @method     updated
+     *
+     * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     *
+     * @param      object           $model    Objeto con información del modelo que genera el evento
+     */
+    public function updated($model)
+    {
+        $this->setNotifications($model, 'updated');
+    }
+
+    /**
+     * Gestiona el evento "deleted" de un modelo
+     *
+     * @method     deleted
+     *
+     * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     *
+     * @param      object           $model    Objeto con información del modelo que genera el evento
+     */
+    public function deleted($model)
+    {
+        $this->setNotifications($model, 'deleted');
+    }
+
+    /**
+     * Gestiona el evento "forceDeleted" de un modelo
+     *
+     * @method     forceDeleted
+     *
+     * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     *
+     * @param      object           $model    Objeto con información del modelo que genera el evento
+     */
+    public function forceDeleted($model)
+    {
+        $this->setNotifications($model, 'forceDeleted');
+    }
+
+    /**
+     * Establece las notificaciones a enviar de acuerdo a la configuración del sistema
+     *
+     * @method     setNotifications
+     *
+     * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     *
+     * @param      object              $model    Objeto con información del modelo que genera el evento observado
+     * @param      string              $type     Tipo de evento generado. created, updated, deleted o forceDeleted
+     */
+    public function setNotifications($model, $type)
+    {
+        /** @var object Obtiene la configuración del modelo del cual enviar una notificación */
+        $notifySetting = NotificationSetting::where('model', $model)->first();
+
+        if ($notifySetting) {
+            /** @var string Nombre del evento o modelo a notificar */
+            $eventName = $notifySetting->name;
+            /** @var string Tipo de evento a notificar. creado, actualizado, eliminado o eliminado permanentemente */
+            $eventType = '';
+            /** @var string Descripción corta del evento a notificar */
+            $event = '';
+
+            switch ($type) {
+                case 'created':
+                    $eventType = __('creado(a)');
+                    $event = __('un registro');
+                    break;
+                case 'updated':
+                    $eventType = __('actualizado(a)');
+                    $event = __("una actualización");
+                    break;
+                case 'deleted':
+                    $eventType = __('eliminado(a)');
+                    $event = __("una eliminación");
+                    break;
+                case 'forceDeleted':
+                    $eventType = __('eliminado(a) permanentemente');
+                    $event = __("una eliminación permanente");
+                    break;
+            }
+
+            if (!empty($eventType) && !empty($event)) {
+                /** @var string Título de la notificación a enviar */
+                $title = "{$eventName} {$eventType}";
+                /** @var string Detalle o descripción de la notificación a enviar */
+                $details = "Se realizó {$event} de datos en {$eventName}";
+
+                foreach ($notifySetting->users() as $user) {
+                    /** Notificación al usuario configurado para recibir notificaciones del sistema */
+                    $user->notify(new SystemNotification($title, $details));
+                }
+            }
+        }
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Models\NotificationSetting;
+use App\Observers\ModelObserver;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,5 +28,18 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        /*foreach (\Module::collections(1) as $module) {
+            foreach (NotificationSetting::where('module', $module->getLowerName())->get() as $notifySetting) {
+                $notifySetting::observe(ModelObserve::class);
+                dd("entro");
+            }
+        }*/
+        foreach (NotificationSetting::all() as $notifySetting) {
+            if (!is_null($notifySetting->module) && \Module::isDisabled($notify->module)) {
+                continue;
+            }
+            ($notifySetting->model)::observe(ModelObserver::class);
+        }
     }
 }
