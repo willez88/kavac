ad54a06cfe70fe180396bcea4e82125d13f0d6f3
Henry Paredes <henryp2804@gmail.com>
2020-04-01 15:30:37 -0430

    Correción en carga masiva de productos de almacén

--- modules/Warehouse/Imports/WarehouseProductImport.php
+++ modules/Warehouse/Imports/WarehouseProductImport.php
@@ -3,6 +3,7 @@
 namespace Modules\Warehouse\Imports;
 
 use Modules\Warehouse\Models\WarehouseProduct;
+use App\Models\MeasurementUnit;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class WarehouseProductImport extends \App\Imports\DataImport implements
@@ -15,11 +16,26 @@ class WarehouseProductImport extends \App\Imports\DataImport implements
     */
     public function model(array $row)
     {
+        /** @var array Datos de la unidad de medida a la cual asociar la información del producto */
+        $dataMeasurementUnit = [
+            'name'        => $row['measurement_unit'],
+            'acronym'     => $row['measurement_unit_acronym'],
+            'description' => $row['measurement_unit_description']
+        ];
+
+        if (empty($row['measurement_unit_id']) || is_null($row['measurement_unit_id'])) {
+            /** @var object Crea la nueva unidad de medida a ser asociado el producto */
+            $measurementUnit = MeasurementUnit::create($dataMeasurementUnit);
+        } else {
+            /** @var object Contiene los datos de la unidad de medida asociada al producto */
+            $measurementUnit = MeasurementUnit::find($row['$measurement_unit_id']);
+        }
+
         /** @var array Datos de los productos a importar */
         $data = [
-            'name'        => $row['name'],
-            'description' => $row['description'],
-            'measurement_unit' => $row['measurement_unit']
+            'name'                => $row['name'],
+            'description'         => $row['description'],
+            'measurement_unit_id' => $measurementUnit->id
         ];
 
         if (!empty($row['id']) || !is_null($row['id'])) {
