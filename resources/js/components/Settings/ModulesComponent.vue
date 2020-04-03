ab46c129193d07cebb6842b8140eccd77a41d8c4
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2020-04-03 22:31:36 +0000

    ajustes

--- resources/js/components/Settings/ModulesComponent.vue
+++ resources/js/components/Settings/ModulesComponent.vue
@@ -1,44 +1,5 @@
 <template>
     <div class="row">
-        <!--<div class="col-2">
-            <div class="card mb-4" style="min-height:100%">
-                <div class="card-header text-center" style="padding-bottom: 15px;color: #0073b7;">
-                    Estatus
-                </div>
-                <div class="list-group list-group-flush log-menu">
-                    <a href="javascript:void(0)" data-toggle="tooltip" title=""
-                       class="list-group-item list-group-item-action d-flex justify-content-between align-items-center active" data-original-title="Todos los módulos">
-                        <span class="level-name">
-                            <i class="fa fa-fw fa-list"></i> Todos
-                        </span>
-                    </a>
-                    <a href="javascript:void(0)" data-toggle="tooltip" title=""
-                       class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" data-original-title="Módulos sin instalar">
-                        <span class="level-name">
-                            <i class="fa fa-fw fa-list"></i> Sin Instalar
-                        </span>
-                    </a>
-                    <a href="javascript:void(0)" data-toggle="tooltip" title=""
-                       class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" data-original-title="Módulos instalados">
-                        <span class="level-name">
-                            <i class="fa fa-fw fa-list"></i> Instalados
-                        </span>
-                    </a>
-                    <a href="javascript:void(0)" data-toggle="tooltip" title=""
-                       class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" data-original-title="Módulos instalados">
-                        <span class="level-name">
-                            <i class="fa fa-fw fa-list"></i> Inhabilitados
-                        </span>
-                    </a>
-                    <a href="javascript:void(0)" data-toggle="tooltip" title=""
-                       class="list-group-item list-group-item-action d-flex justify-content-between align-items-center" data-original-title="Módulos instalados">
-                        <span class="level-name">
-                            <i class="fa fa-fw fa-list"></i> Habilitados
-                        </span>
-                    </a>
-                </div>
-            </div>
-        </div>-->
         <div class="col-12 mb-4" v-if="showDetail">
             <div class="row">
                 <div class="col-4">
@@ -164,99 +125,14 @@
                 </div>
             </div>
         </div>
-        <a class="btn btn-primary btn-show-modal" data-toggle="modal" href='#modal_details'></a>
-        <div class="modal fade" id="modal_details">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h6 class="modal-title card-title">{{ details.name }}</h6>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row">
-                            <div class="col-2">
-                                <img src="/images/default-avatar.png" alt="" class="img-responsive">
-                            </div>
-                            <div class="col-10">
-                                <div class="row">
-                                    <div class="col-12">
-                                        <b>Versión:</b> {{ details.version }}
-                                    </div>
-                                    <div class="col-12">
-                                        <b>Descripción:</b> {{ details.description }}
-                                    </div>
-                                    <div class="col-12">
-                                        <b>Requerimientos:</b>
-                                        <ul>
-                                            <li v-for="(requirement, index) in details.requirements">
-                                                {{ index }} - v{{ requirement }}
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-12">
-                                        <b>Autor(es):</b>
-                                        <ul>
-                                            <li v-for="author in details.authors">
-                                                {{ author.name }}
-                                                [<span v-for="email in author.email" class="span-email">({{ email }})</span>]
-                                            </li>
-                                        </ul>
-                                    </div>
-                                    <div class="col-12" v-if="(details.links!=='')">
-                                        <b>Sitio Web:</b>
-                                        <a :href="details.link" target="_blank" data-toggle="tooltip"
-                                           data-original-title="Sitio web del módulo">{{ details.link }}</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <hr>
-                        <div class="row">
-                            <div class="col-12">
-                                <b>Estatus:</b>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="modal-footer">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
-                        <button type="button" class="btn btn-primary">Instalar</button>
-                    </div>
-                </div>
-            </div>
-        </div>
     </div>
 </template>
 
 <style>
-    .list-group-item:hover {
-        text-decoration: none
-    }
-    .list-group-item .media .media-left img {
-        height:60px;
-    }
-    small {
-        font-size: 70%;
-    }
-    .btn-show-modal {
-        display:none;
-    }
-    #modal_details .modal-dialog {
-        max-width: 60%;
-        margin:10% auto;
-    }
-    #modal_details .modal-title {
-        margin-top: 0
-    }
-    #modal_details img {
-        height: 80px;
-    }
-    .span-email {
-        margin-left: 5px;
-        margin-right: 5px;
-    }
-    .btn-sm {
-        font-size: 0.8571em;
-    }
+    small {font-size: 70%;}
+    .list-group-item:hover {text-decoration: none}
+    .list-group-item .media .media-left img {height:60px;}
+    .btn-sm {font-size: 0.8571em;}
 </style>
 
 <script>
@@ -269,6 +145,15 @@
         },
         props: ['modules'],
         methods: {
+            /**
+             * Obtiene información acerca del módulo seleccionado
+             *
+             * @method     viewDetails
+             *
+             * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+             *
+             * @param      {string}         module    Nombre del módulo del cual se desea obtener información
+             */
             viewDetails(module) {
                 let vm = this;
                 axios.post(`${window.app_url}/modules/details`, {
@@ -276,16 +161,33 @@
                 }).then(response => {
                     vm.details = response.data.details;
                     vm.showDetail = true;
-                    //$(".btn-show-modal").click();
                 }).catch(error => {
                     console.warn(error);
                 });
             },
+            /**
+             * Reinicia los valores para ver detalles de los módulos
+             *
+             * @method     resetDetails
+             *
+             * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+             */
             resetDetails() {
                 let vm = this;
                 vm.showDetail = false,
                 vm.details = {};
             },
+            /**
+             * Verifica si se cumplen o no los requerimientos del módulo
+             *
+             * @method     checkRequirement
+             *
+             * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+             *
+             * @param      {string}     moduleName    Nombre del módulo del cual se van a verificar los requerimientos
+             *
+             * @return     {string}     Estilo a mostrar en el icono que representa si se cumple el requerimiento
+             */
             checkRequirement(moduleName) {
                 return 'fa fa-check-square-o';
             }
