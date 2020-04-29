104d06eb4dea70805eb1f2cc53290d835bbfef98
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2020-04-29 21:05:13 +0000

    optimización del módulo Budget para carga dinámica de componentes Vuejs

--- modules/Budget/Resources/assets/js/app.js
+++ modules/Budget/Resources/assets/js/app.js
@@ -3,47 +3,49 @@
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
  */
-Vue.component('budget-accounts-list', require('./components/BudgetAccountsListComponent.vue').default);
+Vue.component('budget-accounts-list', () => import('./components/BudgetAccountsListComponent.vue'));
 
 /**
  * Componente para mostrar listado de proyectos
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
  */
-Vue.component('budget-projects-list', require('./components/BudgetProjectsListComponent.vue').default);
+Vue.component('budget-projects-list', () => import('./components/BudgetProjectsListComponent.vue'));
 
 /**
  * Componente para mostrar listado de acciones centralizadas
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
  */
-Vue.component('budget-centralized-actions-list', require('./components/BudgetCentralizedActionsListComponent.vue').default);
+Vue.component('budget-centralized-actions-list', () => import('./components/BudgetCentralizedActionsListComponent.vue'));
 
 /**
  * Componente para mostrar listado de acciones centralizadas
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
  */
-Vue.component('budget-specific-actions-list', require('./components/BudgetSpecificActionsListComponent.vue').default);
+Vue.component('budget-specific-actions-list', () => import('./components/BudgetSpecificActionsListComponent.vue'));
 
 /**
  * Componente para mostrar listado de formulaciones de presupuesto
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
  */
-Vue.component('budget-formulation-list', require('./components/BudgetSubSpecificFormulationListComponent.vue').default);
+Vue.component('budget-formulation-list', () => import('./components/BudgetSubSpecificFormulationListComponent.vue'));
 
 /**
  * Componente para mostrar formulario de formulación de presupuesto por sub específica
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
  */
-Vue.component('budget-sub-specific-formulation', require('./components/BudgetSubSpecificFormulationComponent.vue').default);
+Vue.component('budget-sub-specific-formulation', () => import('./components/BudgetSubSpecificFormulationComponent.vue'));
 
 /**
  * Componente para getionar las modificaciones presupuestarias
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+ *
+ * @todo Problema al cargar con lazy load
  */
 Vue.component('budget-mod', require('./components/BudgetModificationComponent.vue').default);
 
@@ -51,6 +53,8 @@ Vue.component('budget-mod', require('./components/BudgetModificationComponent.vu
  * Componente para mostrar listado de créditos adicionales
  *
  * @author Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+ *
+ * @todo Problema al cargar con lazy load
  */
 Vue.component('budget-mod-list', require('./components/BudgetModificationListComponent.vue').default);
 
