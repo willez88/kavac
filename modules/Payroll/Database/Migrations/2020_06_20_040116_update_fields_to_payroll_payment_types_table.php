2d74a4dd27422a98759c474401d30fefe5696784
Henry Paredes <henryp2804@gmail.com>
2020-06-30 10:57:01 -0400

    Gestión de tipos de pagos de nómina

---
+++ modules/Payroll/Database/Migrations/2020_06_20_040116_update_fields_to_payroll_payment_types_table.php
@@ -0,0 +1,68 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * @class      UpdateFieldsToPayrollPaymentTypesTable
+ * @brief      Actualiza los campos de la tabla de tipos de pago de nómina
+ *
+ * Gestiona la creación o eliminación de los campos de la tabla de tipos de pago de nómina
+ *
+ * @author     Henry Paredes <hparedes@cenditel.gob.ve>
+ * @license    <a href='http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/'>
+ *                 LICENCIA DE SOFTWARE CENDITEL
+ *             </a>
+ */
+class UpdateFieldsToPayrollPaymentTypesTable extends Migration
+{
+    /**
+     * Método que ejecuta las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('payroll_payment_types')) {
+            Schema::table('payroll_payment_types', function (Blueprint $table) {
+                if (Schema::hasColumn('payroll_payment_types', 'code')) {
+                    $table->unique('code');
+                }
+                if (!Schema::hasColumn('payroll_payment_types', 'budget_account_id')) {
+                    $table->foreignId('budget_account_id')->nullable()
+                          ->comment('Identificador único asociado a la cuenta presupuestaria')
+                          ->constrained()->onDelete('restrict')->onUpdate('cascade');
+                }
+                if (!Schema::hasColumn('payroll_payment_types', 'associated_records')) {
+                    $table->longText('associated_records')->nullable()
+                          ->comment('Campos del expediente del trabajador a usar en la relación de pago');
+                }
+
+            });
+        };
+    }
+
+    /**
+     * Método que elimina las migraciones
+     *
+     * @author Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('payroll_payment_types', function (Blueprint $table) {
+            if (Schema::hasColumn('payroll_payment_types', 'code')) {
+                $table->dropUnique(['code']);
+            }
+            if (Schema::hasColumn('payroll_payment_types', 'budget_account_id')) {
+                $table->dropForeign(['budget_account_id']);
+                $table->dropColumn('budget_account_id');
+            }
+            if (Schema::hasColumn('payroll_payment_types', 'associated_records')) {
+                $table->dropColumn('associated_records');
+            }
+        });
+    }
+}
---
+++ modules/Payroll/Database/Migrations/2020_06_20_044914_create_payroll_concept_payment_type_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * @class      CreatePayrollConceptPaymentTypeTable
+ * @brief      Crear tabla intermedia entre conceptos y tipos de pago de nómina
+ *
+ * Gestiona la creación o eliminación de la tabla intermedia entre conceptos y tipos de pago de nómina
+ *
+ * @author     Henry Paredes <hparedes@cenditel.gob.ve>
+ * @license    <a href='http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/'>
+ *                 LICENCIA DE SOFTWARE CENDITEL
+ *             </a>
+ */
+class CreatePayrollConceptPaymentTypeTable extends Migration
+{
+    /**
+     * Método que ejecuta las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('payroll_concept_payment_type')) {
+            Schema::create('payroll_concept_payment_type', function (Blueprint $table) {
+                $table->id()->comment('Identificador único del registro');
+
+                $table->foreignId('payroll_concept_id')->comment('Identificador único asociado al concepto')
+                      ->constrained()->onDelete('restrict')->onUpdate('cascade');
+                $table->foreignId('payroll_payment_type_id')->comment('Identificador único asociado al tipo de pago')
+                      ->constrained()->onDelete('restrict')->onUpdate('cascade');
+                
+                $table->timestamps();
+            });
+        };
+    }
+
+    /**
+     * Método que elimina las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payroll_concept_payment_type');
+    }
+}
