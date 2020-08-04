0d706490f8c7b6ee571cf3ce52a880218f192ead
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2020-08-03 20:25:12 -0400

    correcciones en migración

--- modules/DigitalSignature/Database/Migrations/2020_08_02_170935_create_signprofiles_table.php
+++ modules/DigitalSignature/Database/Migrations/2020_08_02_170935_create_signprofiles_table.php
@@ -15,11 +15,9 @@ class CreateSignprofilesTable extends Migration
     {
         Schema::create('signprofiles', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('user_id')->unsigned()->nullable()
-                      ->comment('Identificador del usuario');
-            $table->foreign('user_id')->references('id')->on('users')->onUpdate('cascade');
+            $table->foreignId('user_id')->constrained()->onUpdate('cascade')->comment('Identificador del usuario');
             $table->string('cert', 3000)->comment('Certificado firmante');
-            $table->string('pkey',2000)->comment('Clave privada asociada al certificado del firmante');
+            $table->string('pkey', 2000)->comment('Clave privada asociada al certificado del firmante');
             $table->timestamps();
             $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
         });
