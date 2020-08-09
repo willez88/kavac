176e57d1a01ce0b05811ca2fea691eb97ce6e451
Henry Paredes <henryp2804@gmail.com>
2020-08-09 04:02:55 +0100

    Correción en funciones editar y eliminar en configuración de nómina

--- modules/Payroll/Resources/assets/js/components/settings/PayrollParametersComponent.vue
+++ modules/Payroll/Resources/assets/js/components/settings/PayrollParametersComponent.vue
@@ -277,7 +277,7 @@
                                         title="Modificar registro" data-toggle="tooltip" type="button">
                                     <i class="fa fa-edit"></i>
                                 </button>
-                                <button @click="deleteRecord(props.row.id, 'parameters')"
+                                <button @click="deleteRecord(props.index, 'parameters')"
                                         class="btn btn-danger btn-xs btn-icon btn-action"
                                         title="Eliminar registro" data-toggle="tooltip"
                                         type="button">
