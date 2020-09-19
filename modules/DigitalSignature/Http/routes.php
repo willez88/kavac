c5af8ac12113995e3d64beedf687c9ba789ac822
Angelo Osorio <danielking.321@gmail.com>
2020-09-18 21:49:06 -0400

    Cambios en la vista principal del módulo de firmado
    - Los botones de subir, actualizar y eliminar certificado están funcionales
    - Los botones de firmar y detalles del certificados aún no son funcionales

--- modules/DigitalSignature/Http/routes.php
+++ modules/DigitalSignature/Http/routes.php
@@ -15,7 +15,7 @@ Route::group(
         Route::get('signFile', 'DigitalSignatureController@signFile')->name('signFile');
         Route::get('verifysign', 'DigitalSignatureController@verifysign')->name('verifysign');
         Route::get('listCertificate', 'DigitalSignatureController@listCertificate')->name('listCertificate');
-        Route::get('deletecertificate', 'DigitalSignatureController@destroy')->name('deletecertificate');
+        Route::get('deleteCertificate', 'DigitalSignatureController@destroy')->name('deleteCertificate');
     }
 );
 
--- modules/DigitalSignature/Resources/views/index.blade.php
+++ modules/DigitalSignature/Resources/views/index.blade.php
@@ -16,40 +16,7 @@
     {{ __('Firma electrónica') }}
 @stop
 
-
 @section('content')
-
-<div class="col-12">
-    <div class="card">
-        <div class="card-header">
-            <h6 class="card-title"> Acciones Comúnes </h6>
-            <div class="card-btns">
-                <a href="#" data-toggle="tooltip" class="card-minimize btn btn-card-action btn-round"
-                   data-original-title="Minimize Panel">
-                    <i class="now-ui-icons arrows-1_minimal-up"></i>
-                </a>
-            </div>
-        </div>
-        <div class="card-body">
-            <div class="row">
-                <div class="col-xs-2 text-center">
-                    <a href="#" title="Registros de departamentos" data-toggle="tooltip"
-                       class="btn-simplex btn-simplex-md btn-simplex-primary">
-                        <i class="icofont icofont-file-pdf ico-3x"></i>
-                        <span class="pt-2">Firmar PDF</span>
-                    </a>
-                </div>
-                <div class="col-xs-2 text-center">
-                    <a href="{{ route('fileprofile') }}" title="Cargar el certificado del usuario" data-toggle="tooltip"
-                       class="btn-simplex btn-simplex-md btn-simplex-primary">
-                        <i class="icofont icofont-upload-alt ico-3x"></i>
-                        <span> Cargar certificado </span>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 
 <div class="col-12">
     <div class="card">
@@ -90,35 +57,224 @@
 
             @if($cert == 'true')
             <div class="col-md-12">
-                <a class="btn btn-warning btn-xs btn-icon btn-action" href="#"> <i class="fa fa-edit"></i> </a>
-                <a class="btn btn-danger btn-xs btn-icon btn-action" href="#"> <i class="fa fa-trash-o"></i> </a>
-                <a class="btn btn-info btn-xs btn-icon btn-action" href="{{ route('certificateDetails') }}"
-                   data-toggle="tooltip" title="Detalles del certificado">
+                <a class="btn btn-warning btn-xs btn-icon btn-action" href="#" data-target="#modalUpdateCert"
+                   data-toggle="modal" title="Actualizar certificado">
+                   <i class="fa fa-edit"></i>
+               </a>
+                <div class="modal fade" id="modalUpdateCert" tabindex="-1" aria-labelledby="modalUpdateCertLabel"
+                     aria-hidden="true">
+                    <div class="modal-dialog vue-crud">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h6> <i class="icofont icofont-upload-alt inline-block"></i> Actualizar certificado</h6>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <form method="POST" enctype="multipart/form-data" accept-charset="UTF-8"
+                                      action="{{ route('updateCertificate') }}">
+                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                    <p>
+                                        <label for="pkcs12">Actualizar Certificado firmante</label>
+                                        <input id="pkcs12" type="file" class="form-control" name="pkcs12"  required />
+                                    </p>
+                                    <p>
+                                        <label for="phasepass">Contraseña del certificado</label>
+                                        <input id="phasepass" class="form-control" type="password" name="password" required
+                                               placeholder="XXXXXX" />
+                                    </p>
+                                    <p class="text-right">
+                                        <button type="submit" class="btn btn-success btn-icon btn-round"
+                                                data-original-title="Subir certificado">
+                                            <i class="icofont icofont-upload-alt"></i>
+                                        </button>
+                                    </p>
+                                </form>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default btn-sm btn-round btn-modal-close"
+                                        data-dismiss="modal">
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <a class="btn btn-danger btn-xs btn-icon btn-action" href="#" data-target="#modalConfirmDelete"
+                   data-toggle="modal" title="Eliminar certificado">
+                    <i class="fa fa-trash-o"></i>
+                </a>
+                <div class="modal fade" id="modalConfirmDelete" tabindex="-1" aria-labelledby="modalConfirmDeleteLabel"
+                     aria-hidden="true">
+                    <div class="modal-dialog vue-crud">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h6><i class="fa fa-times inline-block"></i> Eliminar certificado</h6>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p class="pb-3"> ¿Está seguro de querer eliminar el certificado? </p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default btn-modal-close"
+                                        data-dismiss="modal">
+                                    <i class="fa fa-times"></i> Cancelar
+                                </button>
+                                <a class="btn btn-primary" href="{{ route('deleteCertificate') }}">
+                                    <i class="fa fa-check"></i> Confirmar
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <a class="btn btn-info btn-xs btn-icon btn-action" href="#" data-target="#modalDetailCert"
+                   data-toggle="modal" title="Detalles del certificado">
                     <i class="fa fa-eye"></i>
                 </a>
+                <div class="modal fade" id="modalDetailCert" tabindex="-1" aria-labelledby="modalDetailCertLabel"
+                     aria-hidden="true">
+                    <div class="modal-dialog vue-crud">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h6><i class="fa fa-certificate"></i> Detalles del certificado</h6>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                  <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                <p> Details from the certificate. </p>
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-default btn-sm btn-round btn-modal-close"
+                                        data-dismiss="modal">
+                                    Cerrar
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             @endif
 
 
-            <div class="d-none">
+            <p class="d-none"> {{ getPathSign('pruebaPDF.pdf') }} </p>
 
-                <p> {{ getPathSign('pruebaPDF.pdf') }} </p>
-                <p>
-                    <span class="font-weight-bold"> Correo: </span>
-                    <span class="text-gray-900"> {{ $user->email }} </span>
-                </p>
+        </div>
+    </div>
+</div>
+
+<div class="col-12">
+    <div class="card">
+        <div class="card-header">
+            <h6 class="card-title"> Acciones Comúnes </h6>
+            <div class="card-btns">
+                <a href="#" data-toggle="tooltip" class="card-minimize btn btn-card-action btn-round"
+                   data-original-title="Minimize Panel">
+                    <i class="now-ui-icons arrows-1_minimal-up"></i>
+                </a>
             </div>
         </div>
-        <div class="card-footer text-right d-none">
-            <button data-toggle="tooltip" type="button"
-                    onclick="window.location.href=&quot;http://localhost:8000/citizenservice/settings&quot;"
-                    class="btn btn-warning btn-icon btn-round" data-original-title="Cancelar y regresar">
-                <i class="fa fa-ban"></i>
-            </button>
-            <button data-toggle="tooltip" id="save" type="submit" class="btn btn-success btn-icon btn-round"
-                    data-original-title="Guardar registro">
-                <i class="fa fa-save"></i>
-            </button>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-xs-2 text-center">
+                    <a class="btn-simplex btn-simplex-md btn-simplex-primary" href="#" data-toggle="modal"
+                       data-target="#modalSign" title="Firmar archivos usando el certificado cargado">
+                        <i class="icofont icofont-file-pdf ico-3x"></i>
+                        <span> Firmar PDF </span>
+                    </a>
+                    <div class="modal fade" id="modalSign" tabindex="-1" aria-labelledby="modalSignLabel"
+                         aria-hidden="true">
+                        <div class="modal-dialog vue-crud">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h6><i class="icofont icofont-file-pdf"></i> Firmar documentos </h6>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                      <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body text-left">
+                                    <form method="POST" enctype="multipart/form-data" accept-charset="UTF-8" action="#">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                        <p>
+                                            <label for="pdf">Cargar PDF a firmar</label>
+                                            <input id="pdf" type="file" class="form-control" required />
+                                        </p>
+                                        <p class="text-right">
+                                            <button type="submit" class="btn btn-success btn-icon btn-round" data-toggle="tooltip"
+                                                    title="Firmar documento">
+                                                <i class="icofont icofont-fountain-pen"></i>
+                                            </button>
+                                        </p>
+                                    </form>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default btn-sm btn-round btn-modal-close"
+                                            data-dismiss="modal">
+                                        Cerrar
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xs-2 text-center">
+                    @if($cert == 'true')
+                        <a class="btn-simplex btn-simplex-md btn-simplex-primary" href="#"
+                           title="Formulario de carga del certificado" data-toggle="modal"
+                           data-target="#modalUpdateCert">
+                            <i class="icofont icofont-upload-alt ico-3x"></i>
+                            <span class="pt-2"> Actualizar certificado </span>
+                        </a>
+                    @else
+                        <a class="btn-simplex btn-simplex-md btn-simplex-primary" href="#"
+                           title="Formulario de carga del certificado" data-toggle="modal"
+                           data-target="#modalUploadCert">
+                            <i class="icofont icofont-upload-alt ico-3x"></i>
+                            <span class="pt-2"> Cargar certificado </span>
+                        </a>
+                    @endif
+                    <div class="modal fade" id="modalUploadCert" tabindex="-1" aria-labelledby="modalUploadCertLabel"
+                         aria-hidden="true">
+                        <div class="modal-dialog vue-crud">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h6> <i class="icofont icofont-upload-alt inline-block"></i> Cargar certificado</h6>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+                                <div class="modal-body text-left">
+                                    <form method="POST" enctype="multipart/form-data" accept-charset="UTF-8"
+                                          action="{{ route('signprofilestore') }}">
+                                        <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                        <p>
+                                            <label for="pkcs12">Cargar Certificado firmante</label>
+                                            <input id="pkcs12" type="file" class="form-control" name="pkcs12"  required />
+                                        </p>
+                                        <p>
+                                            <label for="phasepass">Contraseña del certificado</label>
+                                            <input id="phasepass" class="form-control" type="password" name="password" required
+                                                   placeholder="XXXXXX" />
+                                        </p>
+                                        <p class="text-right">
+                                            <button type="submit" class="btn btn-success btn-icon btn-round"
+                                                    data-original-title="Subir certificado">
+                                                <i class="icofont icofont-upload-alt"></i>
+                                            </button>
+                                        </p>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
