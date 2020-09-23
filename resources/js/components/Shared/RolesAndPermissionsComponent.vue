765abd5525f1465854f343fa7823af937eff779f
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2020-09-22 23:59:37 -0400

    ajustes y actualización de paquetes

--- resources/js/components/Shared/RolesAndPermissionsComponent.vue
+++ resources/js/components/Shared/RolesAndPermissionsComponent.vue
@@ -77,7 +77,22 @@
                 </div>
             </div>
         </div>
-        <div class="card-footer btn-display text-right">
+        <div class="card-footer text-right">
+            <div class="btn-display">
+                <button type="button" @click="reset" class="btn btn-default btn-icon btn-round"
+                        data-toggle="tooltip" title="Borrar datos del formulario">
+                    <i class="fa fa-eraser"></i>
+                </button>
+                <button type="button" @click="redirect_back(route_list)"
+                        class="btn btn-warning btn-icon btn-round" data-toggle="tooltip"
+                        title="Cancelar y regresar">
+                    <i class="fa fa-ban"></i>
+                </button>
+                <button type="button" @click="createRecord(saveUrl, false, false)"
+                        class="btn btn-success btn-icon btn-round" data-toggle="tooltip" title="Guardar registro">
+                    <i class="fa fa-save"></i>
+                </button>
+            </div>
             <button type="button" @click="reset" class="btn btn-default btn-icon btn-round"
                     data-toggle="tooltip" title="Borrar datos del formulario">
                 <i class="fa fa-eraser"></i>
