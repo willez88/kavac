e5ae534252d9f12c7209c1ab8d20e9a2ff9f8e48
Henry Paredes <henryp2804@gmail.com>
2020-10-19 20:33:00 -0400

    Ajustes en formulario de solicitud de vacaciones

---
+++ modules/Payroll/Database/Migrations/2020_10_09_020905_create_payroll_vacation_requests_table.php
@@ -0,0 +1,65 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * @class      CreatePayrollVacationRequestsTable
+ * @brief      Crear tabla de solicitud de vacaciones
+ *
+ * Gestiona la creación o eliminación de la tabla de solicitud de vacaciones
+ *
+ * @author     Henry Paredes <hparedes@cenditel.gob.ve>
+ * @license    <a href='http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/'>
+ *                 LICENCIA DE SOFTWARE CENDITEL
+ *             </a>
+ */
+class CreatePayrollVacationRequestsTable extends Migration
+{
+    /**
+     * Método que ejecuta las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function up()
+    {
+        if (!Schema::hasTable('payroll_vacation_requests')) {
+            Schema::create('payroll_vacation_requests', function (Blueprint $table) {
+                $table->id()->comment('Identificador único del registro');
+                
+                $table->string('code')->nullable()->unique()
+                      ->comment('Código asociado a la solicitud de vacaciones');
+                $table->string('status')
+                      ->comment('Estatus de la solicitud de vacaciones');
+
+                $table->unsignedInteger('days_requested')
+                      ->comment('Días solicitados');
+                $table->unsignedInteger('vacation_period_year')
+                      ->comment('Año del período vacacional');
+
+                $table->date('start_date')->comment('Fecha de inicio del período vacacional');
+                $table->date('end_date')->comment('Fecha de culminación del período vacacional');
+
+                $table->foreignId('payroll_staff_id')
+                      ->comment('Identificador único asociado al trabajador')
+                      ->constrained()->onDelete('restrict')->onUpdate('cascade');
+                
+                $table->timestamps();
+                $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
+            });
+        };
+    }
+
+    /**
+     * Método que elimina las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payroll_vacation_requests');
+    }
+}
---
+++ modules/Payroll/Models/PayrollVacationRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Modules\Payroll\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use OwenIt\Auditing\Contracts\Auditable;
+use OwenIt\Auditing\Auditable as AuditableTrait;
+use App\Traits\ModelsTrait;
+
+/**
+ * @class      PayrollVacationRequest
+ * @brief      Datos de las solicitudes de vacaciones
+ *
+ * Gestiona el modelo de datos de las solicitudes de vacaciones
+ *
+ * @author     Henry Paredes <hparedes@cenditel.gob.ve>
+ * @license    <a href='http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/'>
+ *                 LICENCIA DE SOFTWARE CENDITEL
+ *             </a>
+ */
+class PayrollVacationRequest extends Model implements Auditable
+{
+    use SoftDeletes;
+    use AuditableTrait;
+    use ModelsTrait;
+
+    /**
+     * Lista de atributos para la gestión de fechas
+     * @var array $dates
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * Lista de atributos que pueden ser asignados masivamente
+     * @var array $fillable
+     */
+    protected $fillable = [
+        'code', 'status', 'days_requested', 'vacation_period_year', 'start_date', 'end_date', 'payroll_staff_id'
+    ];
+}
