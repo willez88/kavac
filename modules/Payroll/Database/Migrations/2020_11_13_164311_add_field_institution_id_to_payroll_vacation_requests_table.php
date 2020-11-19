db2fd253edfd96cf7efc0158effbd382ad5fb16e
Henry Paredes <henryp2804@gmail.com>
2020-11-19 03:33:22 -0400

    Agregada gestión de solicitudes de vacaciones

---
+++ modules/Payroll/Database/Migrations/2020_11_13_164311_add_field_institution_id_to_payroll_vacation_requests_table.php
@@ -0,0 +1,56 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * @class      AddFieldInstitutionIdToPayrollVacationRequestsTable
+ * @brief      Agrega el campo institución a la tabla de solicitudes de vacaciones
+ *
+ * Gestiona la creación o eliminación del campo institución a la tabla de solicitudes de vacaciones
+ *
+ * @author     Henry Paredes <hparedes@cenditel.gob.ve>
+ * @license    <a href='http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/'>
+ *                 LICENCIA DE SOFTWARE CENDITEL
+ *             </a>
+ */
+class AddFieldInstitutionIdToPayrollVacationRequestsTable extends Migration
+{
+    /**
+     * Método que ejecuta las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function up()
+    {
+        if (Schema::hasTable('payroll_vacation_requests')) {
+            Schema::table('payroll_vacation_requests', function (Blueprint $table) {
+                if (!Schema::hasColumn('payroll_vacation_requests', 'institution_id')) {
+                    $table->foreignId('institution_id')->nullable()
+                          ->comment('Identificador único asociado a la institución')
+                          ->constrained()->onDelete('restrict')->onUpdate('cascade');
+                };
+            });
+        };
+    }
+
+    /**
+     * Método que elimina las migraciones
+     *
+     * @author    Henry Paredes <hparedes@cenditel.gob.ve>
+     * @return    void
+     */
+    public function down()
+    {
+        if (Schema::hasTable('payroll_vacation_requests')) {
+            Schema::table('payroll_vacation_requests', function (Blueprint $table) {
+                if (Schema::hasColumn('payroll_vacation_requests', 'institution_id')) {
+                    $table->dropForeign(['institution_id']);
+                    $table->dropColumn('institution_id');
+                };
+            });
+        };
+    }
+}
--- modules/Payroll/Resources/assets/js/components/requests/vacations/PayrollVacationRequestListComponent.vue
+++ modules/Payroll/Resources/assets/js/components/requests/vacations/PayrollVacationRequestListComponent.vue
@@ -1,25 +1,51 @@
 <template>
     <section id="payrollVacationRequestListComponent">
         <v-client-table :columns="columns" :data="records" :options="table_options">
+            <div slot="date" slot-scope="props">
+                <span> {{ format_date(props.row.created_at, 'YYYY-MM-DD') }} </span>
+            </div>
+            <div slot="payroll_staff" slot-scope="props">
+                <span>
+                    {{ 
+                        props.row.payroll_staff
+                            ? props.row.payroll_staff.id
+                                ? props.row.payroll_staff.first_name + ' ' + props.row.payroll_staff.last_name
+                                : 'No definido'
+                            : 'No definido'
+
+                    }}
+                </span>
+            </div>
+            <div slot="status" slot-scope="props">
+                <span v-if="props.row.status == 'approved'">
+                    Aprobado
+                </span>
+                <span v-else-if="props.row.status == 'pending'">
+                    Pendiente
+                </span>
+                <span v-else>
+                    Rechazado
+                </span>
+            </div>
             <div slot="id" slot-scope="props" class="text-center">
-                <button @click="showRecord(props.row.id)"
-                        class="btn btn-info btn-xs btn-icon btn-action"
-                        data-toggle="tooltip" title="Ver información del registro"
-                        data-placement="bottom" type="button">
-                    <i class="fa fa-info-circle"></i>
-                </button>
-                <button @click="editForm(props.row.id)"
-                        class="btn btn-warning btn-xs btn-icon btn-action"
-                        data-toggle="tooltip" title="Modificar registro"
-                        data-placement="bottom" type="button">
-                    <i class="fa fa-edit"></i>
-                </button>
-                <button @click="deleteRecord(props.index, '')"
-                        class="btn btn-danger btn-xs btn-icon btn-action btn-tooltip"
-                        title="Eliminar registro" data-toggle="tooltip" data-placement="bottom"
-                        type="button">
-                    <i class="fa fa-trash-o"></i>
-                </button>
+                <div class="d-inline-flex ">
+                    <payroll-vacation-request-show
+                        :route_show="'payroll/vacation-requests/show/' + props.row.id"
+                        :id="props.row.id">
+                    </payroll-vacation-request-show>
+                    <button @click="editForm(props.row.id)"
+                            class="btn btn-warning btn-xs btn-icon btn-action"
+                            data-toggle="tooltip" title="Modificar registro"
+                            data-placement="bottom" type="button">
+                        <i class="fa fa-edit"></i>
+                    </button>
+                    <button @click="deleteRecord(props.index, '')"
+                            class="btn btn-danger btn-xs btn-icon btn-action btn-tooltip"
+                            title="Eliminar registro" data-toggle="tooltip" data-placement="bottom"
+                            type="button">
+                        <i class="fa fa-trash-o"></i>
+                    </button>
+                </div>
             </div>
         </v-client-table>
     </section>
@@ -30,20 +56,20 @@
             return {
                 record:  {},
                 records: [],
-                columns: ['code', 'created_at', 'payroll_staff', 'status', 'id'],
+                columns: ['code', 'date', 'payroll_staff', 'status', 'id'],
             }
         },
         created() {
             const vm = this;
             vm.table_options.headings = {
                 'code':          'Código',
-                'created_at':    'Fecha de la solicitud',
+                'date':    'Fecha de la solicitud',
                 'payroll_staff': 'Trabajador',
                 'status':        'Estatus de la solicitud',
                 'id':            'Acción'
             };
-            vm.table_options.sortable   = ['code', 'created_at', 'payroll_staff', 'status'];
-            vm.table_options.filterable = ['code', 'created_at', 'payroll_staff', 'status'];
+            vm.table_options.sortable   = ['code', 'date', 'payroll_staff', 'status'];
+            vm.table_options.filterable = ['code', 'date', 'payroll_staff', 'status'];
         },
 
         mounted() {
