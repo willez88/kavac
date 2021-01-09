c39033a46d90867ec7a105802adeeee9a1d81589
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2021-01-09 09:28:59 -0400

    ajustes de estilos en componentes de configuración general

--- resources/js/components/Settings/TaxUnitsComponent.vue
+++ resources/js/components/Settings/TaxUnitsComponent.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="col-12 col-sm-6 col-md-4 col-lg-2 mt-2 mb-2 text-center">
+	<div class="col-12 col-sm-6 col-md-4 col-lg-3 col-xl-2 mt-2 mb-2 text-center">
 		<a class="btn-simplex btn-simplex-md btn-simplex-primary"
 		   href="javascript:void(0)" title="Registros de Unidades Tributarias" data-toggle="tooltip"
 		   @click="addRecord('add_tax_unit', 'tax-units', $event)">
