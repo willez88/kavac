7473c4a1d2b24f67718d684d7c16052da40440fc
Miguel Narvaez <miguelnarvaez31@gmail.com>
2021-03-03 10:41:48 -0400

    Se crea modelo y migración de Lista de subservicios

---
+++ modules/Sale/Database/Migrations/2021_03_03_143315_create_sale_list_subservices_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * @class CreateSaleListSubservicesTable
+ * @brief [descripción detallada]
+ *
+ * [descripción corta]
+ *
+ * @author [autor de la clase] [correo del autor]
+ *
+ * @license
+ *     [LICENCIA DE SOFTWARE CENDITEL](http://conocimientolibre.cenditel.gob.ve/licencia-de-software-v-1-3/)
+ */
+class CreateSaleListSubservicesTable extends Migration
+{
+    /**
+     * Ejecuta las migraciones.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_list_subservices', function (Blueprint $table) {
+            $table->id();
+            
+            $table->timestamps();
+            $table->softDeletes()->comment('Fecha y hora en la que el registro fue eliminado');
+        });
+    }
+
+    /**
+     * Revierte las migraciones.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_list_subservices');
+    }
+}
