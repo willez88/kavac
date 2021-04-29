af0b1cd6cac8bc739103482a80cde5cd5620bf44
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2021-04-29 06:51:23 -0400

    ajustes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -177,7 +177,10 @@
                     if (typeof(Inputmask) !== "undefined" && typeof(Inputmask) === "function") {
                         Inputmask().mask(document.querySelectorAll("input"));
                         $('*[placeholder$="XXX-0000000000-YYYY"]').each(function() {
-                            Inputmask({regex: "^[A-Z]{1,3}$-[0]{4,8}-^([Y]{2}|[Y]{4})$"}).mask(this);
+                            Inputmask({
+                                regex: "^[A-Z]{1,3}$-[0]{4,8}-^([Y]{2}|[Y]{4})$",
+                                clearMaskOnLostFocus: true
+                            }).mask(this);
                         });
                     }
 
