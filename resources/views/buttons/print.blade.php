d67954adaefe9695456e2165481d276d386516b5
Ing. Roldan Vargas <rvargas@cenditel.gob.ve>
2021-05-05 09:09:51 -0400

    botón estándar con acciones para imprimir reportes

--- resources/views/buttons/print.blade.php
+++ resources/views/buttons/print.blade.php
@@ -1,4 +1,14 @@
-{!! Form::button('<i class="fa fa-print"></i>', [
-	'class' => 'btn btn-sm btn-primary btn-custom', 'data-toggle' => 'tooltip', 'type' => 'button',
-	'title' => __('Imprimir registro'), 'onclick' => (isset($print['action']))?"print({$print['action']})":'print()'
-]) !!}
+@if (isset($route))
+    <a href="{{ $route }}" class="btn btn-sm btn-primary btn-custom" data-toggle="tooltip"
+       title="{{ __('Imprimir registro') }}" target="_blank">
+        <i class="fa fa-print"></i>
+    </a>
+@else
+    {!! Form::button('<i class="fa fa-print"></i>', [
+        'class' => 'btn btn-sm btn-primary btn-custom', 'data-toggle' => 'tooltip', 'type' => 'button',
+        'title' => __('Imprimir registro'),
+        'onclick' => (isset($print['action']))
+                     ? "print({$print['action']})"
+                     : ((isset($print['function']))?$print['function']:'print()')
+    ]) !!}
+@endif
