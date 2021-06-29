118b027594a178d9456cb121cf7304e4c640d100
William Páez <paez.william8@gmail.com>
2021-06-29 04:03:42 -0400

    Definición de métodos store, destroy y getDocument del DocumentController de la aplicación base

--- app/Http/Controllers/DocumentController.php
+++ app/Http/Controllers/DocumentController.php
@@ -119,11 +119,55 @@ class DocumentController extends Controller
      * @method    destroy
      *
      * @author     Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     * @author     William Páez <wpaez@cenditel.gob.ve> | <paez.william8@gmail.com>
      *
      * @param     Document    $document    Objeto con información del documento a eliminar
+     * @param     integer     $id         Identificador del documento a eliminar
+     *
+     * @return    JsonResponse     JSON con información del resultado en la eliminación del documento
      */
-    public function destroy(Document $document)
+    public function destroy(Request $request, $id)
     {
-        //
+        /** @var Document Objeto con información del documento a eliminar */
+        $document = Document::find($id);
+
+        if (is_null($document)) {
+            return response()->json([
+                'result' => false, 'message' => __('El documento no existe o ya fue eliminado')
+            ], 200);
+        }
+
+        /** @var string Ruta del archivo a eliminar */
+        $file = $document->file;
+
+        DB::transaction(function () use ($document, $file, $request) {
+            if ($request->force_delete) {
+                $document->forceDelete();
+                if (Storage::disk((isset($request->store)) ? $request->store : 'documents')->exists($file)) {
+                    Storage::disk((isset($request->store)) ? $request->store : 'documents')->delete($file);
+                }
+            } else {
+                $document->delete();
+            }
+        });
+        return response()->json(['result' => true, 'message' => 'Success'], 200);
+    }
+
+    /**
+     * Obtiene detalles de un documento
+     *
+     * @method  getDocument
+     *
+     * @author  Ing. Roldan Vargas <rvargas@cenditel.gob.ve> | <roldandvg@gmail.com>
+     * @author  William Páez <wpaez@cenditel.gob.ve> | <paez.william8@gmail.com>
+     *
+     * @param  Request    $request    Datos de la petición
+     * @param  Document   $document   Objeto con los datos del documento
+     *
+     * @return JsonResponse             JSON con los detalles del documento consultado
+     */
+    public function getDocument(Request $request, Document $document)
+    {
+        return response()->json(['result' => true, 'document' => $document], 200);
     }
 }
