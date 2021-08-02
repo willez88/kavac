e3032fb266a00dbb01b70c1818761f187b1192eb
Miguel Narvaez <miguelnarvaez31@gmail.com>
2021-08-02 13:21:32 -0400

    Se modifica vista de pagos en comercialización

--- modules/Sale/Resources/views/payment/list.blade.php
+++ modules/Sale/Resources/views/payment/list.blade.php
@@ -17,23 +17,125 @@
 @stop
 
 @section('content')
-	<div class="row">
-        <div class="col-12">
-            <div class="card">
-                <div class="card-header">
-                    <h6 class="card-title">Pagos Registrados</h6>
-                    <div class="card-btns">
-                        @include('buttons.previous', ['route' => url()->previous()])
-                        @include('buttons.minimize')
-                    </div>
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="card-title">Pagos Registrados</h6>
+                <div class="card-btns">
+                    @include('buttons.previous', ['route' => url()->previous()])
+                    {!! Form::button('<i class="fa fa-download"></i>', [
+                        'class'       => 'btn btn-sm btn-primary btn-custom',
+                        'data-toggle' => 'tooltip',
+                        'type'        => 'button',
+                        'title'       => __('Descargar'),
+                        'onclick'     => 'exportData()'
+                    ]) !!}                    
+                    @include('buttons.new', ['route' => route('warehouse.movement.create')])
+                    @include('buttons.minimize')
                 </div>
-			    <div class="card-body">
-				  <div class="row">
-                    {{-- link --}}
-                  </div>
-                </div>
+            </div>
+		    <div class="card-body">
+			  <div class="row">
+                {{-- link --}}
+              </div>
             </div>
         </div>
     </div>
+</div>
 
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="card-title">Pagos pendientes</h6>
+                <div class="card-btns">
+                    @include('buttons.previous', ['route' => url()->previous()])
+                    {!! Form::button('<i class="fa fa-download"></i>', [
+                        'class'       => 'btn btn-sm btn-primary btn-custom',
+                        'data-toggle' => 'tooltip',
+                        'type'        => 'button',
+                        'title'       => __('Descargar'),
+                        'onclick'     => 'exportData()'
+                    ]) !!}     
+                    @include('buttons.new', ['route' => route('warehouse.movement.create')])
+                    @include('buttons.minimize')
+                </div>
+            </div>
+            <div class="card-body">
+              <div class="row">
+                {{-- link --}}
+              </div>
+            </div>
+        </div>
+    </div>
+</div>                
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="card-title">Pagos Aprobados</h6>
+                <div class="card-btns">
+                    @include('buttons.previous', ['route' => url()->previous()])
+                    {!! Form::button('<i class="fa fa-download"></i>', [
+                        'class'       => 'btn btn-sm btn-primary btn-custom',
+                        'data-toggle' => 'tooltip',
+                        'type'        => 'button',
+                        'title'       => __('Descargar'),
+                        'onclick'     => 'exportData()'
+                    ]) !!}     
+                    @include('buttons.new', ['route' => route('warehouse.movement.create')])                    
+                    @include('buttons.minimize')
+                </div>
+            </div>  
+            <div class="card-body">
+              <div class="row">
+                {{-- link --}}
+              </div>
+            </div>
+        </div>
+    </div>
+</div>     
+
+<div class="row">
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h6 class="card-title">Pagos de Anticipos Aprobados</h6>
+                <div class="card-btns">
+                    @include('buttons.previous', ['route' => url()->previous()])
+                    {!! Form::button('<i class="fa fa-download"></i>', [
+                        'class'       => 'btn btn-sm btn-primary btn-custom',
+                        'data-toggle' => 'tooltip',
+                        'type'        => 'button',
+                        'title'       => __('Descargar'),
+                        'onclick'     => 'exportData()'
+                    ]) !!}     
+                    @include('buttons.new', ['route' => route('warehouse.movement.create')])                    
+                    @include('buttons.minimize')
+                </div>
+            </div>  
+            <div class="card-body">
+              <div class="row">
+                {{-- link --}}
+              </div>
+            </div>
+        </div>
+    </div>
+</div>     
 @stop
+
+@section('extra-js')
+    @parent
+    <script>
+        function exportData() {
+            location.href = '/sale/.......';
+        };
+    </script>
+@endsection
+
+                              
+
+
+
