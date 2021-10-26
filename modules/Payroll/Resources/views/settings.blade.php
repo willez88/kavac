656f55a0972b976077912eadd2ab02bd47c23064
Argenis Osorio <argenisosorio580@hotmail.com>
2021-10-26 12:38:49 -0400

    Eliminados componentes genéricos de firma electrónica del panel de registros comúnes de Talento humano

--- modules/Payroll/Resources/views/settings.blade.php
+++ modules/Payroll/Resources/views/settings.blade.php
@@ -233,13 +233,6 @@
 
 					{{-- Discapacidades --}}
 					<payroll-disabilities></payroll-disabilities>
-
-
-					@if ($enable)
-						<testing-component></testing-component>
-
-						<signing-component></signing-component>
-					@endif
 				</div>
 			</div>
 		</div>
