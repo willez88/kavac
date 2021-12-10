4b0079f9f7354dd7ca4f6c7b7cd162ee7ac7064e
Ing. Roldan Vargas <roldandvg@gmail.com>
2021-12-10 07:21:22 -0400

    agregada regla de validación para unicidad para código de Estado de un Pais

---
+++ app/Rules/UniqueEstateCode.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use App\Models\Estate;
+
+class UniqueEstateCode implements Rule, DataAwareRule
+{
+    /**
+     * All of the data under validation.
+     *
+     * @var array
+     */
+    protected $data = [];
+
+    /**
+     * Create a new rule instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Set the data under validation.
+     *
+     * @param  array  $data
+     * @return $this
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        $estate = Estate::where(['country_id' => $this->data['country_id'], 'code' => $value])->first();
+
+        return !$estate;
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Él código del Estado ya existe para el Pais seleccionado.';
+    }
+}
