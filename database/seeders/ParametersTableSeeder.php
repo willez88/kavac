2cb317be04809473fe97390a01fda7896880cf67
Ing. Roldan Vargas <roldandvg@gmail.com>
2022-01-13 22:39:43 -0400

    ajustes en invocacion de clase

--- database/seeders/ParametersTableSeeder.php
+++ database/seeders/ParametersTableSeeder.php
@@ -1,10 +1,10 @@
 <?php
 namespace Database\Seeders;
 
-use Illuminate\Database\Seeder;
-use Illuminate\Database\Eloquent\Model;
 use App\Models\Parameter;
-use DB;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Model;
 
 /**
  * @class ParametersTableSeeder
